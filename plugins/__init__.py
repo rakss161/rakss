4fb6405b99513b69e70d0eb4b3c9979208060cde
rakss161 <milomanis21@gmail.com>
2025-01-11 16:36:10 +0700

    Add files via upload

---
+++ plugins/__init__.py
@@ -0,0 +1,14 @@
+list_available_commands = [
+    "batch",
+    "broadcast",
+    "bc",
+    "log",
+    "ping",
+    "privacy",
+    "start",
+    "stop",
+    "users",
+    "uptime",
+]
+
+__all__ = ["list_available_commands"]
